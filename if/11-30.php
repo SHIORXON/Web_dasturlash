293ed310b8182ea97c7b659d8309c6879c0edb39
Shiorxon Obloqulov <shiorxonobloqulov@gmail.com>
2024-04-05 23:52:25 +0500

    11 dan 20 gacha
    ifning davomi

---
+++ if/11-30.php
@@ -0,0 +1,113 @@
+<?php
+/*
+//if 11
+$a=2;
+$b=1;
+if($a!=$b)
+{
+     $a=max($a,$b);
+     $b=max($a,$b);
+}
+else
+{
+    $b=0; $a=0;
+}
+echo $a." ".$b;
+*/
+
+/*
+//if 12
+$a=2;
+$b=1;
+$c=3;
+if($a>$b and $a>$c)
+{
+    echo $a." kattasi";
+}
+else if($c>$b and $c>$a)
+{
+    echo $c." kattasi";
+}
+else
+{
+   echo $b." kattasi";
+}
+*/
+
+/*
+//if 13
+$a=2;
+$b=1;
+$c=3;
+if($a>$b and $a>$c)
+{
+    echo max($b,$c)." o'rtachasi";
+}
+else if($c>$b and $c>$a)
+{
+    echo max($b,$a)." o'rtachasi";
+}
+else
+{
+   echo max($a,$c)." o'rtachasi";
+}
+*/
+
+/*
+//if 16
+$a=2;
+$b=1;
+$c=3;
+if($a<$b and $b<$c)
+{
+    echo ($a*2). " ". ($b*2). " ". ($c*2);
+}
+else
+{
+   echo -$a. " ".-$b. " ".-$c;
+}
+*/
+
+/*
+//if 18
+$a=2;
+$b=2;
+$c=3;
+if($a==$b)
+{
+    echo 3;
+}
+else if($c==$a)
+{
+    echo 2;
+}
+else
+{
+   echo 1";
+}
+*/
+
+/*
+//if 20
+$x=2; $y=4;
+$x1=1; $y=8;
+$x2=3; $y=9;
+$AB=sqrt(pow((x-x1),2)+pow((y-y1),2));
+$AC=sqrt(pow((x-x2),2)+pow((y-y2),2));
+if($AB>$AC)
+{
+    echo "C";
+}
+else if($AB<$AC)
+{
+    echo "B";
+}
+else 
+{
+    echo "B va C";
+}
+
+*/
+
+
+?>
